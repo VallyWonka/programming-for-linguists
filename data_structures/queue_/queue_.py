8a7af256f35e33ebfe6968e7d9e8b4ace491ebc0
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2021-05-26 14:00:39 +0300

    Added priority to queue

--- data_structures/queue_/queue_.py
+++ data_structures/queue_/queue_.py
@@ -3,8 +3,9 @@ Programming for linguists
 
 Implementation of the data structure "Queue"
 """
-
+import math
 from typing import Iterable
+from collections import defaultdict
 
 
 # pylint: disable=invalid-name
@@ -15,22 +16,36 @@ class Queue_:
 
     # pylint: disable=unused-argument,missing-module-docstring
     def __init__(self, data: Iterable = (), max_size: int = None):
-        self.data = []
+        self.data = defaultdict(list)
         for element in data:
-            self.data.insert(0, element)
+            try:
+                element, priority = element
+            except TypeError:
+                self.data[math.inf].insert(0, element)
+            else:
+                self.data[priority].insert(0, element)
+        self.min_priority = self._set_min_priority()
 
-    def put(self, element):
+    def put(self, element, priority: int = math.inf):
         """
-        Add the element ‘element’ at the end of queue_
+        Add the element ‘element’ at the end of queue_ with the priority value of ‘priority’
         :param element: element to add to queue_
+        :param priority: priority of the element
         """
-        self.data.insert(0, element)
+        self.data[priority].insert(0, element)
+        if priority < self.min_priority:
+            self.min_priority = priority
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        return self.data.pop(-1)
+        item = self.top()
+        self.data[self.min_priority].pop(-1)
+        if not self.data[self.min_priority]:
+            del self.data[self.min_priority]
+            self.min_priority = self._set_min_priority()
+        return item
 
     def empty(self) -> bool:
         """
@@ -54,14 +69,14 @@ class Queue_:
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        return len(self.data)
+        return len(self.data.values())
 
     def top(self):
         """
         Return the first element in queue_
         :return: Item from queue_
         """
-        return self.data[-1]
+        return self.data[self.min_priority][-1]
 
     # pylint: disable=no-self-use
     def capacity(self) -> int:
@@ -70,3 +85,6 @@ class Queue_:
         :return: Maximal size of queue_
         """
         return 0
+
+    def _set_min_priority(self):
+        return min(self.data.keys()) if self.data else math.inf
